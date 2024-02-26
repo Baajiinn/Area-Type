6117d050fcdf911785db1e1476f36b61e2280709
Baajiinn <craftesdras@gmail.com>
2024-02-26 03:21:53 +0100

    Day 1 Done

--- assets/php/nav.php
+++ assets/php/nav.php
@@ -3,20 +3,18 @@
     <!-- Font-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
 </head>
 <nav>
-    <p class="nav_l">Area Type</p>
+    <a href="index.php" class="nav_l">Area Type</a>
     <ul class="nav_c">
         <li><a href="#">Solo</a></li>
         <li><a href="#">Multiplayer</a></li>
         <li><a href="#">Loadboard</a></li>
+        <li><img src="assets/svg/shop.png" style="background-position:center; height: 35px; background-color:#e34c26; border-radius:5px; padding:5px;" alt=""></li>
     </ul>
     <ul class="nav_r">
-        <li><img src="assets/svg/shop.png"
-                style="background-position:center; height: 35px; background-color:#e34c26; border-radius:5px; padding:5px;"
-                alt=""></li>
+
         <li>
             <select class="country" id="slick" name="">
                 <option class="country-items" value="Fr" data-imagesrc="assets/svg/Fr.png"></option>
@@ -36,8 +34,8 @@
 <script type="text/javascript" src="https://cdn.rawgit.com/prashantchaudhary/ddslick/master/jquery.ddslick.min.js">
 </script>
 <script>
-$("#slick").ddslick({
-    width: "100%",
-    imagePosition: "left",
-});
+    $("#slick").ddslick({
+        width: "100%",
+        imagePosition: "left",
+    });
 </script>
