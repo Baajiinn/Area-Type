9d6ca9ab08ca0ef9f48861352601b4a77233499e
Baajiinn <craftesdras@gmail.com>
2024-02-26 03:26:00 +0100

    Day 1 Donee

--- index.php
+++ index.php
@@ -11,14 +11,14 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
         rel="stylesheet">
-    <?php 
-    include './assets/php/nav.php';
-    ?>
+
 
 </head>
 
 <body>
-
+    <?php 
+    include './assets/php/nav.php';
+    ?>
 </body>
 
 </html>
