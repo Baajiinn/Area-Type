570fa7e318da122cc4a311ba32195f9bd16c885a
Baajiinn <craftesdras@gmail.com>
2024-03-03 18:28:23 +0100

    Exercice system Part 1

--- solo/exercice.php
+++ solo/exercice.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/exercice.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,21 +11,26 @@
 <body>
     <?php include '../assets/php/nav2.php' ?>
     <section class="exercice">
-        <div class="container">
-            <p class="untyped" id="texteNonTape">
-                Sous un ciel azur la mer étincelait berçant les vagues en un ballet apaisant Les mouettes
-                planaient créant des arabesques aériennes Sur la plage dorée des coquillages gisaient tels des trésors
-                échoués Un paisible tableau maritime s'offrait invitant à la contemplation
-            </p>
-            <br>
-            <p class="typed">Prout</p>
-            <br>
-            <p class="fault">Prout</p>
+        <div id="header">
+            <div id="info">30</div>
+        </div>
 
+        <div class="container" id="game" tabindex="0">
+            <div id="words"></div>
+            <div id="cursor"></div>
+            <div id="focus-error">Click here to focus</div>
+        </div>
+
+        <div id="footer">
+            <div id="buttons">
+                <button>restart</button>
+                <button>play-pause</button>
+                <button>next</button>
+            </div>
         </div>
     </section>
 
-
+    <script src="../assets/js/exercice.js"></script>
 </body>
 
 </html>
